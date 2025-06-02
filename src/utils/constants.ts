d32d4b19206fd4f9cb2e96b27668e9b01bbb4a27
OmerBugraOz <oomerbugraoz@gmail.com>
2025-06-02 13:14:11 +0300

    feat: add icon

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,11 @@ export const allRoutes = [
   {
     path: '/tracker',
     name: 'Tracker',
+    icon: 'pi pi-chart-line',
   },
   {
     path: '/assets',
     name: 'Assets',
+    icon: 'pi pi-wallet',
   },
 ]
